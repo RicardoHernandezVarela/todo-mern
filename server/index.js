5a3850eabb0e28039c4cc61bef7b11377905369e
RicardoHernandezVarela <rhvarela011192@gmail.com>
2021-08-07 18:35:27 -0500

    Start client and server for mern project

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const mongodb =  require('./dbkeys');
+
+// Express app conf
+const app = express();
+dotenv.config();
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+// Mongoose conf
+mongoose.set('useFindAndModify', false);
+
+// mongodb connection
+mongoose.connect(mongodb, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('connected to the db ✌');
+        app.listen(PORT);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get('/', (req, res) => {
+    res.send('Welcome MF!');
+});
